28fcf40755bf9e38ceafcf5653ecd1015eb4d4d5
User Taylor <elexus0201@gmail.com>
2025-04-07 22:03:30 -0400

    basic server set up

--- server.js
+++ server.js
@@ -6,3 +6,32 @@ const morgan = require('morgan'); // HTTP request logger middleware
 const cors = require("cors"); //Enable Cross-Origin Resource Sharing
 require("dotenv").config(); // Loads environment variables from a .env file
 const cookieParser = require("cookie-parser"); // Read and Work with cookies
+
+const PORT = process.env.PORT||4000;
+
+const app = express();
+
+const liveReloadServer = livereload.createServer();
+
+liveReloadServer.server.once("connection", () => {
+    // wait for nodemon to fully restart before refreshing the page
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+
+app.use(express.static('public'));
+// Use the connect-livereload package to connect nodemon and livereload
+app.use(connectLiveReload());
+// Body parser: used for POST/PUT/PATCH routes: 
+// this will take incoming strings from the body that are URL encoded and parse them into an object that can be accessed in the request parameter as a property called body (req.body).
+app.use(express.urlencoded({ extended: true })); // parse incoming requests with URL payloads (actual data that is sent) and makes the data available in req.body
+app.use(express.json()); // Parses incoming JSON requests
+
+app.use(morgan('tiny')); // tiny logging format
+
+// App Listen
+app.listen(PORT, ()=> {
+    console.log(`Listening to port ${PORT}`);
+  }); 
